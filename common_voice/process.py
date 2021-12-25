40ec285c087031005352b9bd09b0d6c5db316343
laubonghaudoi <laubonghaudoi@icloud.com>
2021-12-25 15:50:04 -0500

    整理〇二 qq 句子

--- common_voice/process.py
+++ common_voice/process.py
@@ -1,13 +1,24 @@
 import re
-import pandas as pd
 
-han_regex = re.compile(r'[\u3006\u3007\u4e00-\u9fff\u3400-\u4dbf\U00020000-\U0002a6df\U0002a700-\U0002ebef\U00030000-\U0003134f]')
-is_han = lambda c: bool(han_regex.fullmatch(c))
+# 判斷係唔係漢字
+han_regex = re.compile(
+    r'[\u3006\u3007\u4e00-\u9fff\u3400-\u4dbf\U00020000-\U0002a6df\U0002a700-\U0002ebef\U00030000-\U0003134f]')
+punc_regex = re.compile(r'[，。？！：；“”]')
+def is_han(c): return bool(han_regex.fullmatch(c) or punc_regex.fullmatch(c))
 
-lines = open("unreviewed.csv", encoding="utf-8").readlines()
+
+lines = open("./unreviewed.csv", encoding="utf-8").readlines()
+lines = list(set(lines))
+
+output = open("./unreviewed.txt", "w", encoding="utf-8")
 
 for line in lines:
-  line = line.strip().split()
-  if len(line) < 5 or len(line) >30:
-    continue
-  else:
+    line = list(line.strip())
+    length = len(line)
+
+    if length < 5 or length > 30:
+        continue
+    else:
+        num_han = sum([is_han(c) for c in line])
+        if num_han / length > 0.9:
+            output.writelines("".join(line) + "\n")
